efc5bdf19c3dbe617a8379b906185d2064c98e9d
sudong0701 <1006364428@qq.com>
2021-01-20 09:07:44 +0800

    解决跨域请求

--- build/dev-server.js
+++ build/dev-server.js
@@ -113,15 +113,15 @@ app.use("/static/ueditor/ue", ueditor(path.join(__dirname, '../public/upload'), 
 
 require('../server/index')(app)
 
-//设置允许跨域访问该服务.
-app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Content-Type', 'application/json;charset=utf-8');
+/*解决请求跨域请求*/
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
     if(req.method=="OPTIONS") res.send(200); //让options请求快速返回
-    else  next()
-});
+    else  next();
+  });
 
 
 //拦截器
