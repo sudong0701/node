553efed666c1faf2de9ba2e27f0507f76e3735a0
sudong.duan <1006364428@qq.com>
2021-09-08 11:02:16 +0800

    <feat>:用户登录密码进行MD5加密

--- original/src/controller/user.js
+++ original/src/controller/user.js
@@ -1,11 +1,17 @@
 const { exec, escape } = require('../db/mysql')
+const { genPasssword } = require('../utils/cryp')
 
 const login = (username, password) => {
 	username = escape(username)   //防止sql注入
 	password = escape(password)   //防止sql注入 
+	console.log(password)
+	//生成加密算法
+	password = genPasssword(password)
+	console.log(password)
 	const sql = `
-		select username, realname from users where username=${username} and password=${password}
+		select username, realname from users where \`username\`=${username} and \`password\`="${password}"
 	`
+	console.log(sql)
 	return exec(sql).then((rows) => {
 		return rows[0] || {}
 	})
